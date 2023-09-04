637c035518609a4c9368fd50cf52fc08bee929ec
Carlos Checa <carloschecaa@gmail.com>
2023-09-04 03:04:42 +0200

    js 1 line

--- wasm-utils/utils.js
+++ wasm-utils/utils.js
@@ -1,7 +1,2 @@
-export async function read_json(path) {
-    return (await fetch(path)).json();
-}
-
-export async function read_image(path) {
-    return await (await fetch(path)).blob();
-}
+export const read_json = async (path) => (await fetch(path)).json();
+export const read_image = async (path) => await (await fetch(path)).blob();
